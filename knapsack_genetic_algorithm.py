9bc228a98c60dc29cc1fc96ebf905a01738a6a45
Atharv Tarte <athrvtrte@gmail.com>
2023-12-21 22:13:29 +0530

    Update knapsack_genetic_algorithm.py

--- knapsack_genetic_algorithm.py
+++ knapsack_genetic_algorithm.py
@@ -1,12 +1,10 @@
 import random
 
-# The indexes for data array
-# 0 = name, 1 = weight, 2 = value, 3 = fitness
+
 KNAPSACK_ITEM_NAME_INDEX = 0
 KNAPSACK_ITEM_WEIGHT_INDEX = 1
 KNAPSACK_ITEM_VALUE_INDEX = 2
 
-# Small knapsack dataset
 # knapsack_items = [
 #     ['Pearls', 3, 4],
 #     ['Gold', 7, 7],
@@ -18,7 +16,7 @@ KNAPSACK_ITEM_VALUE_INDEX = 2
 #     ['Cup', 3, 1],
 # ]
 
-# Large knapsack dataset
+# Large knapsack 
 knapsack_items = [
     ['Axe', 32252, 68674],
     ['Bronze coin', 225790, 471010],
@@ -48,34 +46,16 @@ knapsack_items = [
     ['Zinc cup', 978724, 2100851, 0]
 ]
 
-# The best knapsack score from the brute force approach
+
 BEST_LARGE_KNAPSACK_SCORE = 13692887
 
-# Genetic algorithms are used to evaluate large search spaces for a good solution. It is important to note that a
-# genetic algorithm is not guaranteed to find the absolute best solution. It attempts to find the global best whilst
-# avoiding local best solutions. The general lifecycle of a genetic algorithm is as follows:
+ 
 
-# - Creation of a population: This involves creating a random population of potential solutions.
-
-# - Measuring fitness of individuals in the population: This involves determining how good a specific solution is.
-# This is accomplished by using a fitness function which scores solutions to determine how good they are.
-
-# - Selecting parents based on their fitness: This involves selecting a number of pairs of parents that will reproduce
-# offspring.
-
-# - Reproducing individuals from parents: This involves creating offspring from their respective parents by mixing
-# genetic information and applying slight mutations to the offspring.
-
-# - Populating the next generation: This involves selecting individuals and offspring from the population that will
-# survive to the next generation.
-
-# The indexes for an individual's properties
 INDIVIDUAL_CHROMOSOME_INDEX = 0
 INDIVIDUAL_FITNESS_INDEX = 1
 INDIVIDUAL_PROBABILITY_INDEX = 2
 
 
-# Generate an initial population of random individuals
 def generate_initial_population(population_size):
     population = []
     for individual in range(0, population_size):
@@ -84,7 +64,6 @@ def generate_initial_population(population_size):
     return population
 
 
-# Calculate the fitness for each individual in the population given the maximum weight
 def calculate_population_fitness(population, maximum_weight):
     best_fitness = 0
     for individual in population:
@@ -97,7 +76,6 @@ def calculate_population_fitness(population, maximum_weight):
     return best_fitness
 
 
-# Calculate the fitness for an individual
 def calculate_individual_fitness(individual, maximum_weight):
     total_individual_weight = 0
     total_individual_value = 0
@@ -111,14 +89,12 @@ def calculate_individual_fitness(individual, maximum_weight):
     return total_individual_value
 
 
-# Set the probabilities for selection for each individual in the population
 def set_probabilities(population):
     population_sum = sum(individual[INDIVIDUAL_FITNESS_INDEX] for individual in population)
     for individual in population:
         individual[INDIVIDUAL_PROBABILITY_INDEX] = individual[INDIVIDUAL_FITNESS_INDEX] / population_sum
 
 
-# Roulette wheel selection to select individuals in a population
 def roulette_wheel_selection(population, number_of_selections):
     set_probabilities(population)
     slices = []
@@ -135,21 +111,18 @@ def roulette_wheel_selection(population, number_of_selections):
     return chosen_ones
 
 
-# Reproduce children given two individuals using one point crossover
 def one_point_crossover(parent_a, parent_b, xover_point):
     children = [parent_a[:xover_point] + parent_b[xover_point:],
                 parent_b[:xover_point] + parent_a[xover_point:]]
     return children
 
 
-# Reproduce children given two individuals using two point crossover
 def two_point_crossover(parent_a, parent_b, xover_point_1, xover_point_2):
     children = [parent_a[:xover_point_1] + parent_b[xover_point_1:xover_point_2] + parent_a[xover_point_2:],
                 parent_b[:xover_point_1] + parent_a[xover_point_1:xover_point_2] + parent_b[xover_point_2:]]
     return children
 
 
-# Randomly mutate children
 def mutate_children(children, mutation_rate):
     for child in children:
         random_index = random.randint(0, mutation_rate)
@@ -164,7 +137,6 @@ def mutate_children(children, mutation_rate):
     return children
 
 
-# Reproduce children given selected individuals
 def reproduce_children(chosen_selections):
     children = []
     for parent_index in range(len(chosen_selections)//2 - 1):
@@ -174,7 +146,6 @@ def reproduce_children(chosen_selections):
     return children
 
 
-# Combine the existing population and newly reproduced children
 def merge_population_and_children(population, children):
     return population + children
 
@@ -189,7 +160,6 @@ MUTATION_RATE = 10
 NUMBER_OF_ITERATIONS = 5
 
 
-# Run the genetic algorithm
 def run_ga():
     best_global_fitness = 0
     global_population = generate_initial_population(INITIAL_POPULATION_SIZE)
@@ -208,19 +178,9 @@ def run_ga():
     print('Accuracy: ', best_global_fitness / BEST_LARGE_KNAPSACK_SCORE * 100)
     print('Final population size: ', len(global_population))
 
-    # calculate_population_fitness(global_population, KNAPSACK_WEIGHT_CAPACITY)
-    # the_chosen = roulette_wheel_selection(global_population, 100)
-    # the_children = reproduce_children(the_chosen)
-    # the_children = mutate_children(the_children)
-    # global_population = merge_population_and_children(global_population, the_children)
-    # global_population = roulette_wheel_selection(global_population, 100)
 
 
-# Run the genetic algorithm for a number of iterations
 for i in range(0, NUMBER_OF_ITERATIONS):
     run_ga()
 
-# print(calculate_individual_fitness('01100100010110001110001001', 6404180))
-# print(calculate_individual_fitness('00110101000100011010001000', 6404180))
-# print(calculate_individual_fitness('11100100110110000100101101', 6404180))
-# print(calculate_individual_fitness('00001000010010101101001001', 6404180))
+
